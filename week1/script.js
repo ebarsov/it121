e53aa64fd947139be8b9f4245aeda525d873b63f
ebarsov <109013764+ebarsov@users.noreply.github.com>
2023-01-08 20:22:07 -0800

    Add files via upload

--- week1/script.js
+++ week1/script.js
@@ -1,1 +1,18 @@
+<script>
 
+let count=0;
+let yearsOfStudy="half";
+let goal="take job interview training course, and also, psychotherapy course to stay in full mental health a :)) "
+
+
+const firstName="Elena";
+const lastName="Barsov";
+const address="Some City, WA 98101"
+
+document.write('My name is ' + firstName + ' ' 
+            + lastName + '. I live in ' 
+            + address + '. I have been at SCC for ' 
+            + yearsOfStudy + ' and plan to ' 
+            + goal + 'after completing my courses.')
+
+</script>
